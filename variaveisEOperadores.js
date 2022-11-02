76f72c74f8269ec3f079aa30e44159eb70c1a4c4
Eliane Coelho <ellianee@hotmail.com>
2022-11-02 13:37:51 -0300

    acrescentado tipos de combustível.

--- variaveisEOperadores.js
+++ variaveisEOperadores.js
@@ -1,20 +1,30 @@
 // Aula
-let variavel = 10/10;
+let variavel = 10 / 10;
 
 console.log(variavel);
 
 /*desafio desta aula
 faça um programa para calcular o valor de combustível gasto em uma viagem.
 você terá três variáveis
-- Gasto médio;
-- Gasto médio de combustível do carro por KM;
-- Distancia em KM; */
+ */
 
-
-const precoCombustivel = 5.79;
-const consumoLitro = 10;
+const precoEtanol = 3.99;
+const precoGasolina = 5.79;
+const consumoLitroGasolina = 10;
+const consumoLitroEtanol = 8;
 const kmPercorrido = 100;
+const tipoCombustivel = 'Etanol';
 
-const litrosConsumidos = kmPercorrido / consumoLitro;
+const litrosConsumidosEtanol = kmPercorrido / consumoLitroEtanol;
+const litrosConsumidosGasolina = kmPercorrido / consumoLitroGasolina;
 
-console.log("Você gastou", litrosConsumidos *precoCombustivel);
+if (tipoCombustivel === 'Etanol') {
+
+    const gastoTotal = litrosConsumidosEtanol * precoEtanol;
+    console.log(gastoTotal.toFixed(2));
+
+} else {
+    const gastoTotal = litrosConsumidosGasolina * precoGasolina;
+    console.log(gastoTotal.toFixed(2));
+}
+
