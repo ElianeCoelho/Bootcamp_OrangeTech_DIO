de9430dd495c29da1a6b46a74e19e37b167b2ce7
Eliane Coelho <ellianee@hotmail.com>
2022-11-02 16:28:52 -0300

    resovendo questionario

--- variaveisEOperadores.js
+++ variaveisEOperadores.js
@@ -28,3 +28,50 @@ if (tipoCombustivel === 'Gasolina') {
     console.log(gastoTotal.toFixed(2));
 }
 
+//Questionário
+/*Tendo em vista o código abaixo, qual seria o 
+    resultado esperado da sua execução no terminal?*/
+const nomeDoProfessor = 'Renan Johannsen de Paula';
+console.log(nomeDoProfessor);
+
+//corrigindo exercício:constantes não permitem alteração do valor atribuído.
+const nomeDoProfessor1 = 'Vitor Johansen Guerra';
+console.log(nomeDoProfessor);
+
+
+
+//testando para responder questionário
+const numero = 5 + 1;
+
+if (numero % 2 === 0) {
+    console.log('Número par');
+} else {
+    console.log('Número ímpar');
+}
+
+
+
+const num = 10;
+const numeroEmTexto = '10';
+console.log(numero === numeroEmTexto);
+
+/*Tendo em vista o código abaixo, qual seria o 
+resultado esperado da sua execução no terminal?*/
+
+let nomeDoAluno = 'Renan Johannsen de Paula';
+console.log(nomeDoAluno);
+
+nomeDoAluno = 'Vitor Johansen Guerra';
+console.log(nomeDoAluno);
+
+/*Estruturas condicionais permitem que um programa execute 
+diferentes comandos de acordo com as condições estabelecidas.
+Mediante o código abaixo qual é o resultado esperado de sua execução 
+pelo terminal?*/
+
+
+const numero3 = 10;
+
+if (numero3 >= 10) {
+    console.log('Sucesso demais!');
+}
