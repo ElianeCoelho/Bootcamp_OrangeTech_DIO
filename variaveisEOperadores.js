17b912e0e17ab826e66fb028006d7cac89db13dd
Eliane Coelho <ellianee@hotmail.com>
2022-11-02 11:11:06 -0300

    first commit

---
+++ variaveisEOperadores.js
@@ -0,0 +1,19 @@
+let variavel = 10/10;
+
+console.log(variavel);
+
+/*desafio desta aula
+faça um programa para calcular o valor de combustível gasto em uma viagem.
+você terá três variáveis
+- Gasto médio;
+- Gasto médio de combustível do carro por KM;
+- Distancia em KM; */
+
+
+const precoCombustivel = 5.79;
+const consumoLitro = 10;
+const kmPercorrido = 100;
+
+const litrosConsumidos = kmPercorrido / consumoLitro;
+
+console.log("Você gastou", litrosConsumidos *precoCombustivel);
